75d7f9990b73776c58f2237e749c99b2ade85840
louisgeek <louisgeek@qq.com>
2021-03-30 16:02:27 +0800

    update

--- app/src/main/java/com/louisgeek/hikvisioncalldemo/AppApplication.java
+++ app/src/main/java/com/louisgeek/hikvisioncalldemo/AppApplication.java
@@ -12,9 +12,9 @@ public class AppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        GreenDaoManager.getInstance().init(this);
+   /*     GreenDaoManager.getInstance().init(this);
         EthernetSetting.getInstance().init(this);
-        HicoreHandler.getInstance().init(this);
+        HicoreHandler.getInstance().init(this);*/
         this.registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
             @Override
             public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
@@ -28,7 +28,7 @@ public class AppApplication extends Application {
 
             @Override
             public void onActivityResumed(@NonNull Activity activity) {
-                //检查是否激活
+               /* //检查是否激活
                 boolean value = IndoorActiveSdk.isActive(this);
                 if (!value) {
                     HikLog.e("“设备未激活”");
@@ -37,7 +37,7 @@ public class AppApplication extends Application {
                     if (result != 10) {
                         HikLog.e("“激活失败”");
                     }
-                }
+                }*/
             }
 
             @Override
